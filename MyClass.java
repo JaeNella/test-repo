87082d1591040018f00cebb39ecdf64828a8f627
Jaelan Allen <jaelanallen@gmail.com>
2018-12-12 17:09:07 -0600

    message

---
+++ MyClass.java
@@ -0,0 +1,8 @@
+class MyClass{
+public static void main (String []args){
+
+    System.out.println("This is just a print statement!!");
+	System.out.println("This is something I decided to add last second!!");
+}
+
+}
